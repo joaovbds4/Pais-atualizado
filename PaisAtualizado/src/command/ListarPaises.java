8f12e7420acfe98c86aa353e0f3b749b1b71ded3
joaovbds <55898458+joaovbds@users.noreply.github.com>
2020-05-26 21:08:50 -0300

    Aula 10

--- PaisAtualizado/src/command/ListarPaises.java
+++ PaisAtualizado/src/command/ListarPaises.java
@@ -1,12 +1,10 @@
-package controller;
+package command;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -14,23 +12,10 @@ import javax.servlet.http.HttpSession;
 import model.Pais;
 import service.PaisService;
 
-/**
- * Servlet implementation class ListarPaisesController
- */
-@WebServlet("/listarpaises.do")
-public class ListarPaisesController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListarPaisesController() {
-        super();
-        
-    }
+public class ListarPaises implements Command {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String chave = request.getParameter("data[search]");
 		String acao = request.getParameter("acao");
 		
@@ -49,10 +34,7 @@ public class ListarPaisesController extends HttpServlet {
 		}
 		RequestDispatcher view = request.getRequestDispatcher("ListarPais.jsp");
 		view.forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
 	}
 
 }
